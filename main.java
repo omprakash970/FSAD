6c4c020b32d42ee816cc0133ef6750795503359b
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-02 07:34:42 +0530

    Bug fix: corrected output message

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git Version Control!");
+        System.out.println("Bug fixed in main program");
     }
 }
